bd1a9d889ee444eb66dd65df54e318d6123dbb66
DrSavauG <dr.savaug@gmail.com>
2021-02-23 23:23:21 +0300

    work-shorted-done

--- src/index.js
+++ src/index.js
@@ -1,25 +1,15 @@
-module.exports = 
-function toReadable (number) {
-    if(number == 0) return 'zero';
+module.exports = function toReadable(number) {
+    if (number == 0) return "zero";
     let d1_19 = ['','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
     let d20_90 = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
-    let result = '';
     let i = number.toString();
-    if(number>=100){
-        result += d1_19[i[0]]+' hundred ';
-        number = Number(i.substring(1));
-        i = number.toString()
-        console.log('number =', number);
+    let result = '';
+    if (number >= 100) {
+        result += d1_19[i[0]] + " hundred ";
+        i = i.substring(1);
+        number = Number(i);
     }
-    if(number<=19) result += d1_19[number];
-    console.log(number>=20);
-    if(number>=20) {
-        console.log(i);
-        result += d20_90[i[0]]+' '+d1_19[i[1]]
-    }
-    
-
-
-return result.trim()
+    if (number <= 19) result += d1_19[number];
+    if (number >= 20) result += d20_90[i[0]] + ' ' + d1_19[i[1]];
+    return result.trim();
 };
-// console.log('toReadable(123) =', toReadable(123));
